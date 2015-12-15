e3bbf21e4b72906259d73e49da7968c9e2f0b2b0
SoflyJade <henon.sophie@wanadoo.fr>
2015-12-15 11:27:40 +0100

    exo

---
+++ script.js
@@ -0,0 +1,20 @@
+var c=document.getElementById("canvas");
+var ctx=c.getContext("2d");
+
+ctx.beginPath();
+
+// cercle
+ctx.arc(200,200,150,0,2*Math.PI);
+ctx.strokeStyle="rgb(0, 161, 208)"; //ligne bleu
+ctx.lineWidth = 15;
+
+// carré 1
+ctx.fillStyle = "rgb(0, 161, 208)";
+ctx.fillRect(173,125,55,55);
+
+// carré 2
+ctx.fillStyle = "rgb(53, 99, 186)";
+ctx.fillRect(173,225,55,55);
+
+
+ctx.stroke();
